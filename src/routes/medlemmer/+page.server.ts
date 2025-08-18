2b17add46d459c14ea7c4e23530a1dfed70b6d96
Ole Magnus Fon Johnsen <me@omfj.no>
2025-08-19 00:01:41 +0200

    Filter not public users

--- src/routes/medlemmer/+page.server.ts
+++ src/routes/medlemmer/+page.server.ts
@@ -2,7 +2,9 @@ import { db } from '$lib/db/drizzle';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	const users = await db.query.users.findMany({
+	let users;
+
+	users = await db.query.users.findMany({
 		columns: {
 			id: true,
 			name: true,
@@ -10,10 +12,16 @@ export const load: PageServerLoad = async ({ locals }) => {
 			imageUrl: true,
 			createdAt: true,
 			activeFrom: true,
-			activeTo: true
+			activeTo: true,
+			isPublic: true
 		},
 		orderBy: (users, { asc }) => [asc(users.name)]
 	});
+
+	// If the user is not logged in, we want to filter out users that are not public
+	if (!locals.user) {
+		users = users.filter((user) => user.isPublic);
+	}
 
 	return {
 		users,
